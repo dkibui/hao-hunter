cbde39319bed1255ea81637359592e56b0ecfd69
David Kibui <dkibui@outlook.com>
2025-01-05 16:32:57 +0300

    Finished CRUD operations for listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,50 @@
 class ListingsController < ApplicationController
+  before_action :set_listing, only: %i[ show edit update destroy ]
+
   def index
     @listings = Listing.all
   end
 
   def show
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.create(listing_params)
+    if @listing.save
+      redirect_to @listing
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @listing.update(listing_params)
+      redirect_to @listing
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @listing.destroy
+    redirect_to listings_path
+  end
+
+  private
+
+  def set_listing
     @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.expect(listing: %i[ title description rent_amount bedrooms
+    bathrooms latitude longitude is_available ])
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ApplicationRecord
   has_rich_text :description
-  validates :title, :bedrooms, :bathrooms,  presence: true
+  validates :title, :description, :bedrooms, :bathrooms, presence: true
+  validates :rent_amount, numericality: true
+  validates :bedrooms, :bathrooms, numericality: { only_integer: true,
+                                                   greater_than_or_equal_to: 0 }
 end
