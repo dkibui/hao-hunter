acd63efc5e348dd31c4251c53514b32dd66812eb
David Kibui <dkibui@outlook.com>
2025-01-14 07:24:15 +0300

    Changed spelling mistake on notification migration

--- db/migrate/20250113173631_create_notifications.rb
+++ db/migrate/20250113173631_create_notifications.rb
@@ -1,8 +1,8 @@
 class CreateNotifications < ActiveRecord::Migration[8.0]
   def change
     create_table :notifications do |t|
-      t.references :user, null: false, foreign_keys: true
-      t.references :listing, null: false, foreign_keys: true
+      t.references :user, null: false, foreign_key: true
+      t.references :listing, null: false, foreign_key: true
       t.string :message, null: false
       t.timestamp :read_at
 
